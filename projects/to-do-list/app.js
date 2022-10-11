cdeb026a2c8485e9c0737f30439fe63fdcd0b8f6
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2022-10-11 12:07:54 +0200

    Improved code by watching the answer video
    all fine, just a few tweaks to improve it

--- projects/to-do-list/app.js
+++ projects/to-do-list/app.js
@@ -1,13 +1,14 @@
 let input = prompt("What would you like to do?");
 
-let list = ["eat", "sleep", "rave", "repeat"];
+const list = ["eat", "sleep", "rave", "repeat"];
 
-while (input !== "quit") {
+while (input !== "quit" && input !== "q") {
   if (input === "add") {
     // Add new item - ask what it is then push onto array
-    input = prompt("What is the new item?");
-    list.push(input);
-    console.log(`${input} added to list.`);
+    const newItem = prompt("What is the new item?");
+    list.push(newItem);
+    console.log(`${newItem} added to list.`);
+    input = null;
   } else if (input === "list") {
     // view list, using for loop and stopping by resetting input to null so it doesn't cycle through again
     for (let i = 0; i < list.length; i++) {
@@ -16,13 +17,21 @@ while (input !== "quit") {
     }
   } else if (input === "delete") {
     // ask which item, then delete it from array:
+    console.log("Here's a reminder of what you currently have on your list:");
+    for (let i = 0; i < list.length; i++) {
+      console.log(`${i}. ${list[i]}`);
+    }
     input = parseInt(
-      prompt("Which item would you like to delete? Type the number.")
+      prompt(
+        "Which item would you like to delete? Type the number (see list in the console)."
+      )
     );
     // Check if it's a number:
-    while (isNaN(input)) {
-      input = prompt(
-        "That's not a number! Please type the number of the item you want to delete."
+    while (Number.isNaN(input)) {
+      input = parseInt(
+        prompt(
+          "That's not a number! Please type the number of the item you want to delete."
+        )
       );
     }
     console.log(`${list[input]} sucessfully deleted.`);
