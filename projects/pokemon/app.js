f8346ea00e298e0d73ae470db992a944553732b0
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2022-10-26 11:54:32 +0200

    Pokemon project for bootcamp

---
+++ projects/pokemon/app.js
@@ -0,0 +1,24 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+const container = document.querySelector("#container");
+
+const baseUrl =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+for (let i = 1; i <= 151; i++) {
+  // Create and append div + give class of 'pokemon'
+  const newDiv = document.createElement("div");
+  container.appendChild(newDiv);
+  newDiv.classList.add("pokemon");
+
+  // Create image
+  const newImg = document.createElement("img");
+  newImg.src = `${baseUrl}${i}.png`;
+
+  // Create span for under the image
+  const newSpan = document.createElement("span");
+  newSpan.innerText = i;
+
+  // Append span and image
+  newDiv.appendChild(newImg);
+  newDiv.appendChild(newSpan);
+}
