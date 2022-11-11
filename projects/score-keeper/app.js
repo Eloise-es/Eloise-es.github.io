8a5ae4714145f43341babfac32a26e1514708db9
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2022-11-11 16:32:35 +0100

    Added serve tracker (2 serves per person)

--- projects/score-keeper/app.js
+++ projects/score-keeper/app.js
@@ -6,11 +6,13 @@ const resetBtn = document.querySelector("#reset");
 const score = document.querySelector("#score");
 const P1Display = document.querySelector("#scoreP1");
 const P2Display = document.querySelector("#scoreP2");
+const serveCountDisplay = document.querySelector("#serveCountDisplay");
 
 // Define starting scores (let so they can change)
 let scorePlayer1 = 0;
 let scorePlayer2 = 0;
 let targetScore = 3;
+let serveCount = 1;
 
 //When the number we're playing to is changed (need to parse as it's a string)
 targetScoreSelect.addEventListener("change", function (e) {
@@ -20,18 +22,16 @@ targetScoreSelect.addEventListener("change", function (e) {
 // On click of reset, change h1 to '0 to 0' and reactivate buttons
 resetBtn.addEventListener("click", reset);
 
-// On click of player 1, add 1 point to total and update h2
+// On click of player 1, add 1 point to total and update score in header, then check game points to update total and check for end of game
 player1Btn.addEventListener("click", function (e) {
   scorePlayer1++;
-  console.log(scorePlayer1);
   P1Display.innerText = scorePlayer1;
   checkGamePoints();
 });
 
-// On click of player 2, add 1 point to total and update h2
+// On click of player 2, add 1 point to total and update score in header, then check game points to update total and check for end of game
 player2Btn.addEventListener("click", function (e) {
   scorePlayer2++;
-  console.log(scorePlayer2);
   P2Display.innerText = scorePlayer2;
   checkGamePoints();
 });
@@ -43,6 +43,27 @@ function checkGamePoints() {
     console.log("game over");
     applyColors();
   }
+  // Count after each point scored to see who should serve (2 serves per person)
+  console.log("serve count", serveCount);
+  serveCount++;
+  if (serveCount === 1) {
+    serveCountDisplay.innerText = "Serve 1 for Player One";
+    serveCountDisplay.className = "";
+    serveCountDisplay.classList.add("text-start", "text-primary");
+  } else if (serveCount === 2) {
+    serveCountDisplay.innerText = "Serve 2 for Player One";
+    serveCountDisplay.className = "";
+    serveCountDisplay.classList.add("text-start", "text-primary");
+  } else if (serveCount === 3) {
+    serveCountDisplay.innerText = "Serve 1 for Player Two";
+    serveCountDisplay.className = "";
+    serveCountDisplay.classList.add("text-end", "text-danger");
+  } else if (serveCount === 4) {
+    serveCountDisplay.innerText = "Serve 2 for Player Two";
+    serveCountDisplay.className = "";
+    serveCountDisplay.classList.add("text-end", "text-danger");
+    serveCount = 0;
+  }
 }
 
 // To reset the score to zero, enable buttons again, reset colours by removing classes
@@ -53,6 +74,10 @@ function reset() {
   P2Display.innerText = scorePlayer2;
   enableButtons();
   resetColors();
+  serveCount = 1;
+  serveCountDisplay.className = "";
+  serveCountDisplay.classList.add("text-start", "text-primary");
+  serveCountDisplay.innerText = "Serve 1";
 }
 
 // Enable and disable buttons
