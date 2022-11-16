88081401d9b0579dc26637ea68f9ea8eced183a9
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2022-11-16 18:25:50 +0100

    Added favourite star wars character

--- projects/fox-generator/app.js
+++ projects/fox-generator/app.js
@@ -1,6 +1,12 @@
 // Declare variables from HTML
 const generateBtn = document.querySelector("#generate-fox-btn");
 const img = document.querySelector("img");
+const foxName = document.querySelector("#fox-name");
+const characterDisplay = document.querySelector("#character");
+const age = document.querySelector("#age");
+const place = document.querySelector("#place");
+const book = document.querySelector("#book");
+const joke = document.querySelector("#joke");
 
 // Add event listener to button
 generateBtn.addEventListener("click", function () {
@@ -11,7 +17,22 @@ generateBtn.addEventListener("click", function () {
 function generateFox() {
   console.log("generating fox....");
   randomFox();
+  getStarWarsPerson(Math.floor(Math.random() * 100));
 }
+
+// Get star wars character from SWAPI
+const getStarWarsPerson = async (id) => {
+  try {
+    const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+    console.log(res.data);
+    const newName = res.data.name;
+    characterDisplay.innerHTML = newName;
+    console.log(newName);
+  } catch (e) {
+    console.log("ERROR", e);
+    characterDisplay.innerHTML = "Not a Star Wars fan";
+  }
+};
 
 // Get random fox from Random Fox API https://apilist.fun/api/randomfox
 const randomFox = async () => {
