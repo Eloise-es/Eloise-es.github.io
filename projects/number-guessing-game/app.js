7ccdd070400ddf9bdb74cb0b18630a82203b76f8
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2022-11-17 14:31:34 +0100

    Improved number quessing game and added comments

--- projects/number-guessing-game/app.js
+++ projects/number-guessing-game/app.js
@@ -1,19 +1,28 @@
 function startGame() {
-  let maximum = parseInt(prompt("Choose a maximum number for this game."));
+  // Prompt for max number (if null (cancel button) then stop process)
+  let maximum = prompt("Choose a maximum number for this game.");
+  if (maximum === null) {
+    return;
+  }
+  // Parse to make it a number (do this after because it needs to check for null)
+  parseInt(maximum);
   console.log(maximum);
 
+  // Check if it's a number, if not, prompt to enter new one
   while (!maximum) {
     maximum = parseInt(prompt(`That is not a number! Please enter a number.`));
   }
-
+  // Create target number
   const targetNum = Math.floor(Math.random() * maximum) + 1;
   console.log(targetNum);
 
-  let guess = parseInt(
-    prompt(`Enter your first guess! It's a number between 1 and ${maximum}...`)
+  //  Prompt first guess (don't parse, so that 'quit' can work)
+  let guess = prompt(
+    `Enter your first guess! It's a number between 1 and ${maximum}...`
   );
   let attempts = 1;
 
+  // While guess is not correct...
   while (parseInt(guess) !== targetNum) {
     if (guess === "quit") break;
     attempts++;
@@ -23,9 +32,12 @@ function startGame() {
       guess = prompt("Nope, that's too high!");
     }
   }
+  // If 'quit' is typed, quit game
   if (guess === "quit") {
     alert(`Oh no!! You quit after only ${attempts} guesses :(`);
-  } else if (parseInt(guess) === targetNum) {
+  }
+  // If correct - response depends on number of guesses it took
+  else if (parseInt(guess) === targetNum) {
     if (attempts === 1) {
       alert(
         `Amazing, you got it in just one guess! The secret number was ${targetNum}.`
