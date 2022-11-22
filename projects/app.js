d3c4488e640ea4cf23bd223ccfcd6b5826de124e
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2022-11-22 10:11:08 +0100

    Create array with 2 projects
    This is just to start and have something to play around with

---
+++ projects/app.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    name: "Banana Quiz",
+    description: "Information page and interactive quiz about bananas",
+    date: new Date(2022 - 04 - 18),
+    topics: ["HTML", "CSS", "Basic JavaScript"],
+    source: `<a href="https://www.shecodes.io/graduates/39228-eloise-salmon"
+                target="_blank"
+                >SheCodes Basics course</a>`,
+    img: "../images/Bananas-screenshot.png",
+  },
+  {
+    name: "To Do List (v1)",
+    description: "Very very basic To Do List",
+    date: new Date(2022 - 10 - 11),
+    topics: ["Basic JavaScript"],
+    source: `<a href="https://www.udemy.com/course/the-web-developer-bootcamp/"
+                target="_blank"
+                >Colt Steele's Web Developer Bootcamp</a>`,
+    img: "../images/to-do-screenshot.png",
+  },
+];
