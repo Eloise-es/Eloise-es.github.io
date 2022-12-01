1249cdadcb55396ba67618719213c1589115dd7b
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2022-12-01 12:10:37 +0100

    Using 'config' variable for API

--- projects/tv-search-app/app.js
+++ projects/tv-search-app/app.js
@@ -6,9 +6,8 @@ const main = document.querySelector("main");
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
-  const res = await axios.get(
-    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
-  );
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get("https://api.tvmaze.com/search/shows?q=", config);
   console.log(res);
   addImages(res.data);
   form.elements.query.value = "";
