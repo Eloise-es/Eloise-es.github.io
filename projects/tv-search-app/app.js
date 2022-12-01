3cf7633dd7a8f2102b46a76cd23d289a9dbd506c
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2022-12-01 12:07:31 +0100

    API using axios

--- projects/tv-search-app/app.js
+++ projects/tv-search-app/app.js
@@ -1,5 +1,14 @@
+// Declare document elements
 const form = document.querySelector("#searchForm");
-form.addEventListener("submit", function (e) {
+const main = document.querySelector("main");
+
+// Add event listener to form
+form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
+  const res = await axios.get(
+    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+  );
+  console.log(res);
+  form.elements.query.value = "";
 });
