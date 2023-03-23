ea2c73629d720ca056337926e420f6207cbd1168
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-03-23 16:02:02 +0100

    Improve projects index

--- js/projects.js
+++ js/projects.js
@@ -5,7 +5,7 @@ const projects = [
     description: "Information page and interactive quiz about bananas.",
     href: "bananas/index.html",
     date: "18th April 2022",
-    topics: ["HTML", "CSS", "Basic JavaScript"],
+    topics: ["HTML", "CSS", "JavaScript"],
     source: `<a href="https://www.shecodes.io/graduates/39228-eloise-salmon"
                 target="_blank"
                 >SheCodes Basics course</a>`,
@@ -18,10 +18,10 @@ const projects = [
       "Basic guessing game created to practise If Statements and While Loops in JavaScript.",
     href: "number-guessing-game/index.html",
     date: "3rd October 2022",
-    topics: ["Basic JavaScript", "While Loops", "If Statements"],
+    topics: ["JavaScript"],
     source: `<a href="https://www.udemy.com/course/the-web-developer-bootcamp/"
                 target="_blank"
-                >Colt Steele's Web Developer Bootcamp</a>`,
+                >Web Developer Bootcamp</a>`,
     img: "../images/number-guessing-game.png",
     imgAltTxt: "Guessing game screenshot",
   },
@@ -30,10 +30,10 @@ const projects = [
     description: "Very very basic To Do List.",
     href: "to-do-list/index.html",
     date: "11th October 2022",
-    topics: ["Basic JavaScript"],
+    topics: ["JavaScript"],
     source: `<a href="https://www.udemy.com/course/the-web-developer-bootcamp/"
                 target="_blank"
-                >Colt Steele's Web Developer Bootcamp</a>`,
+                >Web Developer Bootcamp</a>`,
     img: "../images/to-do-screenshot.png",
     imgAltTxt: "To Do List screenshot",
   },
@@ -43,10 +43,10 @@ const projects = [
       "Pictures of all the original Pokemon, added to the page using JavaScript.",
     href: "pokemon/index.html",
     date: "26th October 2022",
-    topics: ["Bootstrap", "Basic JavaScript", "Loops"],
+    topics: ["Bootstrap", "JavaScript", "CSS"],
     source: `<a href="https://www.udemy.com/course/the-web-developer-bootcamp/"
                 target="_blank"
-                >Colt Steele's Web Developer Bootcamp</a>`,
+                >Web Developer Bootcamp</a>`,
     img: "../images/pokemon-screenshot.png",
     imgAltTxt: "Pikemon screenshot",
   },
@@ -56,10 +56,10 @@ const projects = [
       "Just click and it changes the page to a random colour. That's it!!",
     href: "random-color-generator/index.html",
     date: "26th October 2022",
-    topics: ["Basic JavaScript"],
+    topics: ["JavaScript"],
     source: `<a href="https://www.udemy.com/course/the-web-developer-bootcamp/"
                 target="_blank"
-                >Colt Steele's Web Developer Bootcamp</a>`,
+                >Web Developer Bootcamp</a>`,
     img: "../images/colors-screenshot.png",
     imgAltTxt: "Color generator screenshot",
   },
@@ -72,7 +72,7 @@ const projects = [
     topics: ["JavaScript", "Bootstrap"],
     source: `<a href="https://www.udemy.com/course/the-web-developer-bootcamp/"
                 target="_blank"
-                >Colt Steele's Web Developer Bootcamp</a>`,
+                >Web Developer Bootcamp</a>`,
     img: "../images/ping-pong-screenshot.png",
     imgAltTxt: "Ping Pong Score Keeper screenshot",
   },
@@ -82,7 +82,7 @@ const projects = [
     href: "fox-generator/index.html",
     date: "17th November 2022",
     topics: ["Bootstrap", "JavaScript", "APIs"],
-    source: `My own idea, using free APIs found online`,
+    source: `My own idea, using free APIs`,
     img: "../images/fox-generator-screenshot.png",
     imgAltTxt: "Fox Generator screenshot",
   },
@@ -90,13 +90,26 @@ const projects = [
     name: "TV Show Search",
     description: "Search for TV shows and see useful information about them.",
     href: "tv-search-app/index.html",
-    date: "1st December 2022, updated 6th December 2022",
+    date: "1st-6th December 2022",
     topics: ["Bootstrap", "JavaScript", "APIs"],
     source: `<a href="https://www.udemy.com/course/the-web-developer-bootcamp/"
                 target="_blank"
-                >Colt Steele's Web Developer Bootcamp</a>`,
+                >Web Developer Bootcamp</a>`,
     img: "../images/tv-search-screenshot.png",
-    imgAltTxt: "Fox Generator screenshot",
+    imgAltTxt: "TV show screenshot",
+  },
+  {
+    name: "Yelp Camp",
+    description:
+      "The final project for my course, this full-stack application was built using Node and MongoDB.",
+    href: "https://yelp-camp-eloise.onrender.com",
+    date: "January - March 2023",
+    topics: ["Bootstrap", "JavaScript", "Node.js", "Express", "MongoDB"],
+    source: `<a href="https://www.udemy.com/course/the-web-developer-bootcamp/"
+                target="_blank"
+                >Web Developer Bootcamp</a>`,
+    img: "../images/yelp-camp.jpg",
+    imgAltTxt: "Yelp Camp screenshot",
   },
 ];
 // Select container that cards go into
@@ -104,8 +117,14 @@ const container = document.querySelector("#projectsContainer");
 
 // Iterate over projects in array, adding cards with details
 for (let project of projects) {
+  let topics = "";
+  if (project.topics) {
+    for (let topic of project.topics) {
+      topics += `<span class="badge text-bg-secondary m-1">${topic}</span>`;
+    }
+  }
   const cardContents = `<div class="col my-2">
-            <div class="card h-100 text-bg-light">
+            <div class="card h-100">
             <a href="${project.href}"
                 ><img
                   src="${project.img}"
@@ -117,14 +136,12 @@ for (let project of projects) {
                 <p class="card-text">
                   ${project.description}
                 </p>
-                <p class="card-text">
-                  <small>${project.date}</small>
-                  <br />
-                  <small>
-                    ${project.source}</small
-                  >
-                </p>
+                <div id="card-badges">${topics}</div>
               </div>
+              <ul class="list-group list-group-flush">
+               <li class="list-group-item">${project.source}</li>
+              </ul>
+              <div class="card-footer text-muted">${project.date}</div>
             </div>
             </div>`;
   container.insertAdjacentHTML("afterbegin", cardContents);
